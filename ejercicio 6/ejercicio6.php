05bfb11e552417e6bc24a0a43ad81d2dfdd76d34
William Trujillo <williamtruji67@gmail.com>
2023-12-17 18:37:46 -0500

    variables y constantes

---
+++ ejercicio 6/ejercicio6.php
@@ -0,0 +1,13 @@
+<?php
+    $edad=18;//primer valor de la variable
+
+    echo $edad."<br/>";
+
+    $edad=19;//modificacion del valor de la variable $edad
+    
+    echo $edad."<br/>";
+
+    //constante (no cambia) ejemplo pi, IVA 
+    define("NOMBRE","WILLIAM");
+    echo NOMBRE;
+?>
