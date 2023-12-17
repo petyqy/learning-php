8a523af3713f67e4e7b7c3c81973b320a25a4141
William Trujillo <williamtruji67@gmail.com>
2023-12-17 16:45:56 -0500

    metodo POST formulario basico

---
+++ ejercicio 2/ejercicio2.php
@@ -0,0 +1,26 @@
+<?php 
+    if($_POST){
+
+    
+    // recibe la informacion del formulario html via POST
+    $nombre = $_POST["txNombre"];
+    echo "Hola " . $nombre;
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+    
+    <form action="ejercicio2.php" method="post">
+        Nombre:
+        <input type="text" name="txNombre" id="">
+        <br/>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
