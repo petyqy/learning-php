6fdeb5064d5a618172181c91610285d76e63fcbf
William Trujillo <williamtruji67@gmail.com>
2023-12-17 18:35:48 -0500

    Tipos de datos

---
+++ ejercicio 5/ejercicio5.php
@@ -0,0 +1,8 @@
+<?php
+//tipos de datos
+    $Edad=18;               //int
+    $Nombre="William";       //sting
+    $Altura=1.4;            //float
+    $Programador=null;      //bool
+    echo "El nombre es ".$Nombre." su edad ".$Edad." años de edad, su altura es ".$Altura." mt "." es programador? ".$Programador
+?>
