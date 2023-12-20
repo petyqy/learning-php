cf5973d4f96d6db47c3e71d640f2e60cfaf54115
William Trujillo <williamtruji67@gmail.com>
2023-12-20 11:59:34 -0500

    ejermplo A mayor que B

---
+++ ejercicio 8/ejercicio8.php
@@ -0,0 +1,29 @@
+
+<?php
+//operadores relacionales
+if($_POST){
+    $valorA=$_POST["valorA"];
+    $valorB=$_POST["valorB"];
+    if($valorA > $valorB){
+        echo 'El valor A: '.$valorA.' es mayor que el valor B: '.$valorB;
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores</title>
+</head>
+<body>
+    <form action="ejercicio8.php" method="post">
+        Valor A: 
+    <input type="text" name="valorA" id="">
+        Valor B: 
+    <input type="text" name="valorB" id="">
+        Resultado=
+    <input type="submit" name="resultado" id="">
+    </form>
+</body>
+</html>
