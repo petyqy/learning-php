c66da3ad740fa218d74ad3e77ef42fb67fd9a83d
William Trujillo <williamtruji67@gmail.com>
2023-12-24 18:37:40 -0500

    ciclo while

---
+++ ejercicio 15/ejercicio15.php
@@ -0,0 +1,9 @@
+<?php
+    $numeroInicial=0;
+    //condicion
+    while($numeroInicial<10){
+        echo'Numero '.$numeroInicial.'<br/>';
+        $numeroInicial++;
+        //el ciclo se reincia por cada numero (adicion hasta llegar a 9 o <=10)
+    }
+?>
