e086b42cbc961f4a9c704c09eee4c640f7ec86d4
William Trujillo <williamtruji67@gmail.com>
2023-12-24 18:32:27 -0500

    ciclo do-whuile

---
+++ ejercicio 14/ejercicio14.php
@@ -0,0 +1,13 @@
+<?php 
+    //ciclo do-while
+    //variable
+    $numeroInicial=0;
+    //adicion
+    do{
+        echo'Numero '.$numeroInicial.'<br/>';
+        $numeroInicial++;
+    }
+    //condicion
+    while($numeroInicial<10)
+
+?>
