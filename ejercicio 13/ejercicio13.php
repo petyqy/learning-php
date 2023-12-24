6cf5132d60ae39505b7ab83ecfe17fa45d4d65d5
William Trujillo <williamtruji67@gmail.com>
2023-12-24 18:24:34 -0500

    ciclo for (comentarios)

--- ejercicio 13/ejercicio13.php
+++ ejercicio 13/ejercicio13.php
@@ -1,5 +1,7 @@
 <?php 
     //ciclo for
+    /*Se declara el rango de 0 a menor o igual a
+    10 añadiendo una adicion*/ 
     for ($ninicial=0; $ninicial<=10; $ninicial++ ){
         echo ' Numero '.$ninicial.'<br/>';
     }
