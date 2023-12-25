e55adc22325bdb9f5e605ea009995869a80fe4e6
William Trujillo <williamtruji67@gmail.com>
2023-12-25 17:30:35 -0500

    ciclo foreach

--- ejercicio 22/ejercicio22.php
+++ ejercicio 22/ejercicio22.php
@@ -2,5 +2,9 @@
     //cambiando el indice manualmente
     $frutas=array('f'=>'fresa','m'=>'manzana','p'=>'pera');
     print_r($frutas);
-    echo $frutas['p'];
+    echo $frutas['m'].'<br/>';
+
+    foreach($frutas as $indice=>&$valor){
+       echo  'El valor '.$valor.' Tiene el indice '.$indice.'<br/>';
+    }
 ?>
