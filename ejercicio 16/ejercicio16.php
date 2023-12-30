97460bc24bb13da71843a28ee932608b4bdc83e3
William Trujillo <williamtruji67@gmail.com>
2023-12-30 14:36:33 -0500

    funciones (comentarios)

--- ejercicio 16/ejercicio16.php
+++ ejercicio 16/ejercicio16.php
@@ -1,9 +1,9 @@
 <?php
 //funciones
-            /*apellido tiene un valor nulo o vacio para 
-            no provocar errores
-            sin ese valor seran requeridos todos los argumentos 
-            de manera obligatoria*/
+        /*apellido tiene un valor nulo o vacio para 
+        no provocar errores
+        sin ese valor seran requeridos todos los argumentos 
+        de manera obligatoria*/
     function printname($nombre,$apellido=""){
         //intrucciones o rutinas que seran
         //aplicados en todas los llamados
